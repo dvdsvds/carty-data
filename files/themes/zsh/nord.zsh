1f7384ff839e0898ad536bc88ec5726f7ae02c8a
dvdsvds <kundr5@naver.com>
2026-09-18 16:12:10 +0900

    add test item: nord zsh theme

---
+++ files/themes/zsh/nord.zsh
@@ -0,0 +1,9 @@
+---
+name: Nord
+description: A cold, arctic-inspired zsh theme
+category: themes/zsh
+preview_colors: ["#2e3440", "#3b4252", "#88c0d0"]
+provides:
+    aliases: []
+    function: []
+---
