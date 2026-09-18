b1171b944a3769678e359be87197f41a9c5580b5
dvdsvds <kundr5@naver.com>
2026-09-19 05:50:44 +0900

    add nvim-colorscheme category

---
+++ files/themes/nvim/tokyonight.lua
@@ -0,0 +1,18 @@
+---
+name: Tokyo Night
+description: A clean, dark theme inspired by Tokyo at night
+category: themes/nvim
+preview_colors: ["#1a1b26", "#7aa2f7", "#bb9af7"]
+provides:
+    aliases: []
+    function: []
+---
+{
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = fuction()
+        vim.cmd.colorscheme("tokyonight")
+    end,
+},
