5c787cd62d93986c22a40ca803e711e08e8925a3
dvdsvds <kundr5@naver.com>
2026-09-21 05:17:20 +0900

    git 브랜치 파트: git:(브랜치) 텍스트 대신 아이콘(⎇)+브랜치명, 글자색도 배경색과 별개로 지정 가능하도록 변경

--- files/zsh-custom/git-branch.zsh
+++ files/zsh-custom/git-branch.zsh
@@ -8,6 +8,6 @@ provides:
     functions: []
 depends_on: []
 ---
-ZSH_THEME_GIT_PROMPT_PREFIX="%K{{{color}}}%F{0} git:("
-ZSH_THEME_GIT_PROMPT_SUFFIX=") %k%f "
+ZSH_THEME_GIT_PROMPT_PREFIX="%K{{{color}}}%F{{{font_color}}} ⎇ "
+ZSH_THEME_GIT_PROMPT_SUFFIX=" %k%f "
 PROMPT+='$(git_prompt_info)'
