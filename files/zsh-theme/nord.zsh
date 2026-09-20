a1745887e7312ce60091f0edd50ed5a9f50305c7
dvdsvds <kundr5@naver.com>
2026-09-20 11:54:25 +0900

    test: stale index.json (CI guard verification, will be closed)

--- files/zsh-theme/nord.zsh
+++ files/zsh-theme/nord.zsh
@@ -1,6 +1,6 @@
 ---
 name: Nord
-description: A cold, arctic-inspired zsh theme
+description: A cold, arctic-inspired zsh theme (edited)
 category: zsh-theme
 preview_colors: ["#2e3440", "#3b4252", "#88c0d0"]
 provides:
